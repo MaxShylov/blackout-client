1c371505aae21f71e5bb4b9b6d3c661359d0d9b2
MaxShylov <shma9013@gmail.com>
2022-12-17 16:42:56 +0200

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 
 import { Routes, Route } from 'react-router-dom';
+import './App.module.scss';
 
 import { AddAddress } from './pages/AddAddress';
 
 const App = () => (
   <div>
     <Routes>
+      <Route element={<AddAddress />} path="/menu" />
       <Route element={<AddAddress />} path="/add-address" />
     </Routes>
   </div>
--- src/pages/AddAddress.tsx
+++ src/pages/AddAddress.tsx
@@ -1,3 +1,3 @@
 import React, { FC } from 'react';
 
-export const AddAddress: FC = () => <div>Add Address</div>;
+export const AddAddress: FC = () => <div>Add Address 123</div>;
